b746a6187ae70a6414a5fe8da6c157374b773fac
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-04 13:11:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,4 +27,25 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  
+  search_term = req.params["q"]
+  
+  if @@items.include?(search_term)
+  resp.write "#{search_term} is one of our items"
+  else
+  resp.write "Couldn't find #{search_term}"
+  end
+  
+  else
+  resp.write "Path Not Found"
+  end
+  
+  resp.finish
+  end
+
+
+
+
+  
 end
